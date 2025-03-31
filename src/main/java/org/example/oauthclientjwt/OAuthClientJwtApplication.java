04058a6a5f69c43b010c45f5f4a364ebe5969561
dlawnsdnjs <59200605+dlawnsdnjs@users.noreply.github.com>
2025-03-31 15:56:39 +0900

    dot env add

--- src/main/java/org/example/oauthclientjwt/OAuthClientJwtApplication.java
+++ src/main/java/org/example/oauthclientjwt/OAuthClientJwtApplication.java
@@ -1,12 +1,14 @@
 package org.example.oauthclientjwt;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OAuthClientJwtApplication {
-
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(OAuthClientJwtApplication.class, args);
     }
 
--- src/main/java/org/example/oauthclientjwt/Service/CustomOAuth2UserService.java
+++ src/main/java/org/example/oauthclientjwt/Service/CustomOAuth2UserService.java
@@ -6,9 +6,10 @@ import org.example.oauthclientjwt.Repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final UserRepository userRepository;
 
