2b40401686b77f9b15d36d9a7beb4c6a089d0518
dlawnsdnjs <59200605+dlawnsdnjs@users.noreply.github.com>
2025-04-01 15:59:32 +0900

    jwt filter and cors setting

--- src/main/java/org/example/oauthclientjwt/Configuration/SecurityConfig.java
+++ src/main/java/org/example/oauthclientjwt/Configuration/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.example.oauthclientjwt.Configuration;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.example.oauthclientjwt.JWT.JWTFilter;
 import org.example.oauthclientjwt.JWT.JWTUtil;
 import org.example.oauthclientjwt.OAuth2.CustomSuccessHandler;
 import org.example.oauthclientjwt.Service.CustomOAuth2UserService;
@@ -10,6 +12,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +37,7 @@ public class SecurityConfig {
                 .csrf(auth -> auth.disable())
                 .formLogin(auth -> auth.disable())
                 .httpBasic(auth -> auth.disable())
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService))
@@ -38,7 +46,25 @@ public class SecurityConfig {
                 .requestMatchers("/", "/login/**").permitAll()
                 .anyRequest().authenticated())
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration config = new CorsConfiguration();
+
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+
+                        config.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        config.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return null;
+                    }
+                }));
 
         return http.build();
     }
